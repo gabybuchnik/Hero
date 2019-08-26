2e93e2dc3cc5f690588d26e3853a9c5d8627efcf
gabybuchnik <gabybuchnik@gmail.com>
2019-08-26 11:36:54 +0300

    add service style and rest calls

--- weather-app/src/app/main/main.component.ts
+++ weather-app/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-main',
@@ -7,28 +8,36 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class MainComponent implements OnInit {
   @ViewChild('city') city: ElementRef;
-  @ViewChild('cityOption') cityOption : ElementRef;
-  @ViewChild('citySelect') citySelect : ElementRef;
-  cities : string[];
-  constructor() { 
-    this.cities = ['tel aviv'];
+  @ViewChild('cityOption') cityOption: ElementRef;
+  @ViewChild('citySelect') citySelect: ElementRef;
+  cityName : string;
+  cities;
+  currentWeather;
+  constructor(private weatherService: WeatherService) {
+    this.cities = [{ LocalizedName: 'Tel Aviv', key: '215854' }];
+    this.currentWeather = null;
+    this.cityName ="Tel Aviv";
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.autocomplete();
+    this.getWeather('215854');
   }
-
   async autocomplete() {
-    this.cities = [];
     let city = this.city.nativeElement.value;
-    if (city) {
-      let res = await fetch('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=NaNiOUWhHpyul0z4nQeWs88bZQku8jH7&q=' + city);
-      let data = await res.json();
-      for (let city in data) {
-        this.cities.push(data[city].LocalizedName);
+    this.cities = await this.weatherService.autocomplete(city);
+  }
+
+  setCity() {
+    this.cityName = this.citySelect.nativeElement.options[this.citySelect.nativeElement.options.selectedIndex].value;
+    for(let item of this.cities){
+      if (item.LocalizedName === this.cityName) {
+        this.getWeather(item.Key);
       }
     }
   }
-  setCity(){
-    this.city.nativeElement.value = this.citySelect.nativeElement.options[this.citySelect.nativeElement.options.selectedIndex].value;
+  async getWeather(key){
+    this.currentWeather = await this.weatherService.getCurrentDayWeather(key);
   }
+  
 }
---
+++ weather-app/src/app/weather.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  cities: object[];
+  currentWeather;
+  fiveDaysWeather;
+  api_key: string;
+  
+  constructor() {
+    this.cities = [];
+    this.currentWeather = null;
+    this.fiveDaysWeather = null;
+    this.api_key = 'X11rAKfsylpGhnbG1OqeRY1TOr8YJ4Gk';
+  }
+
+  async autocomplete(cityName: string) {
+    this.cities = [];
+    const url = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=' + this.api_key + '&q=' + cityName;
+    if (cityName) {
+      let data = await this.requsetFetch(url);
+      for (let city of data) {
+        this.cities.push(city);
+      }
+    }
+    return this.cities;
+  }
+
+  getCurrentDayWeather(citykey: string) {
+    const url = 'http://dataservice.accuweather.com/currentconditions/v1/' + citykey + '?apikey=' + this.api_key;
+    if (citykey) {
+      return this.currentWeather = this.requsetFetch(url);;
+    }
+  }
+
+  getFiveDaysWeather(citykey: string) {
+    const url = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/' + citykey + '?apikey=' + this.api_key;
+    if (citykey) {
+      return this.fiveDaysWeather = this.requsetFetch(url);
+    }
+  }
+
+  async requsetFetch(url){
+    let res = await fetch(url);
+    let data = await res.json();
+    return data;
+  }
+}
